d79caab241090cd59f96ba35d335cbabd7e84284
MagicalMogu <1227598289@qq.com>
2023-06-26 14:04:33 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,26 +1,69 @@
 from flask import Flask, url_for, render_template
 from flask import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 
 app = Flask(__name__)
-name = 'Mogu'
-# 十个字典，每个字典里有两个key
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# 设置变量
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)
 
-#渲染index页面，模版文件名是必须传入参数，其他两个参数为了正确渲染。
+
+# 注册命令
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+
+# 命令，创建虚拟数据
+@app.cli.command()
+def forge():
+    name = 'Mogu'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+# 继承自db的model，表示一个表
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+
+# 渲染index页面，模版文件名是必须传入参数，其他两个参数为了正确渲染。
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    # 查询数据库中记录
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', name=user.name, movies=movies)
 
 
 @app.route('/hello')
