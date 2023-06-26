4db0f2d7156fb342d3cf9b41589042fa5d47484c
MagicalMogu <1227598289@qq.com>
2023-06-26 11:37:25 +0800

    add index page

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from flask import escape
 
 app = Flask(__name__)
+name = 'Mogu'
+# 十个字典，每个字典里有两个key
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
-
+#渲染index页面，模版文件名是必须传入参数，其他两个参数为了正确渲染。
 @app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
 @app.route('/hello')
 @app.route('/home')
 def hello_world():  # put application's code here
@@ -14,8 +32,9 @@ def hello_world():  # put application's code here
 # 动态路由传入变量
 @app.route('/user/<name>')
 def user_page(name):
-    #f代表格式化字符串， escape是常见的安全处理
+    # f代表格式化字符串， escape是常见的安全处理
     return f'user: {escape(name)}'
+
 
 @app.route('/test')
 def test_url_for():
